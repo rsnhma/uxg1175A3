22ce34b5df3845e166aacda225cc5640ab7a10be
amee <corvidernity@gmail.com>
2025-07-08 14:23:30 +0800

    char
    more dev

---
+++ Assets/CharManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class CharManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+
+    public List<Sprite> characters = new List<Sprite>();
+    private int selectedchar = 0;
+
+    public GameObject playerchar;
+
+    public void NextOption()
+    {
+        selectedchar = selectedchar + 1;
+        if (selectedchar >= characters.Count)
+        {
+            selectedchar = 0;
+        }
+        sr.sprite = characters[selectedchar];
+    }
+    public void PrevOption()
+    {
+        selectedchar = selectedchar - 1;
+        if (selectedchar < 0)
+        {
+            selectedchar = characters.Count - 1;
+        }
+        sr.sprite = characters[selectedchar];
+    }
+    public void PlayGame()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playerchar, "Assets/selectedchar.prefab");
+        SceneManager.LoadScene(4);
+    }
+}
